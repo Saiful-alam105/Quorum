8aad65ca19e04b7e0ce7f21ac90dbd4990489d0b
Saiful-alam105 <saifulalamsabbir313@gmail.com>
2026-09-12 00:14:54 +0600

    chore: opt into React Router v7 future flags

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,9 @@ import SettingsPage from "@/pages/SettingsPage"
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route element={<AppLayout />}>
